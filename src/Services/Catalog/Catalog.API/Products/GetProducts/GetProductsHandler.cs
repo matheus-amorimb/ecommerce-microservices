64b16879427aa849bcbea3683cceca87bb702318
Matheus Batista <mbatista.sarti@gmail.com>
2024-10-29 14:47:17 -0300

    feat(products): add getProducts feature

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -0,0 +1,15 @@
+namespace Catalog.API.Products.GetProducts;
+
+public record GetProductsQuery() : IQuery<GetProductsResult>;
+public record GetProductsResult(IEnumerable<Product> Products);
+    
+public class GetProductsQueryHandler(IDocumentSession session, ILogger<GetProductsQueryHandler> logger) 
+    : IQueryHandler<GetProductsQuery, GetProductsResult>
+{
+    public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductsQueryHandler.Hadle called with {@query}", query);
+        var products = await session.Query<Product>().ToListAsync(cancellationToken);
+        return new GetProductsResult(products);
+    }
+}
