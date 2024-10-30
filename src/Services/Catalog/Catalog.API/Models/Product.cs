93a21f0fcd197d3d054791a2e6334155c6840400
Matheus Batista <mbatista.sarti@gmail.com>
2024-10-30 09:48:04 -0300

    feat(product): add domain logic to validate and update product

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -1,3 +1,5 @@
+using Catalog.API.Products.UpdateProduct;
+
 namespace Catalog.API.Models;
 
 public class Product
@@ -8,4 +10,20 @@ public class Product
     public string Description { get;  set; } = default!;
     public string ImageUrl { get;  set; } = default!;
     public decimal Price { get;  set; }
+    
+    //Domain logic
+    public void UpdateFromCommand(UpdateProductCommand command)
+    {
+        var productType = typeof(Product);
+        var commandType = command.GetType();
+        
+        foreach(var prop in commandType.GetProperties())
+        {
+            var newValue = prop.GetValue(command, null);
+            if (newValue is not null)
+            {
+                productType.GetProperty(prop.Name)?.SetValue(this, newValue);
+            }
+        }
+    }
 }
