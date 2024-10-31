0e6a2db70b30cd1e0bf206ffd5c74a1ed70e9c78
Matheus Batista <mbatista.sarti@gmail.com>
2024-10-30 23:15:42 -0300

    feat(logging): add logging behavior

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,4 @@
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCarter();
@@ -7,6 +6,7 @@ builder.Services.AddMediatR(configuration =>
 {
     configuration.RegisterServicesFromAssembly(typeof(Program).Assembly);
     configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    configuration.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 builder.Services.AddMarten(options =>
 {
@@ -16,15 +16,12 @@ builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.MapCarter();
 app.UseExceptionHandler(options => { });
 app.UseHttpsRedirection();
-
 app.Run();
