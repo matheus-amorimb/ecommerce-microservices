30067190a1e38df977c359667e05abf602d69571
Matheus Batista <mbatista.sarti@gmail.com>
2024-11-02 10:49:51 -0300

    feat(model): define shopping cart model

---
+++ src/Services/Basket/Basket.API/Models/ShoppingCart.cs
@@ -0,0 +1,14 @@
+namespace Basket.API.Models;
+
+public class ShoppingCart
+{
+    public string Username { get; set; } = default!;
+    public List<ShoppingCartItem> Items { get; set; } = [];
+    public decimal TotalPrice => Items.Sum(item => item.Price * item.Quantity);
+
+    public ShoppingCart(string username)
+    {
+        Username = username;
+    }
+    public ShoppingCart(){}
+}
