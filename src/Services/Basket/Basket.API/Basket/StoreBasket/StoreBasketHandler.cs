a5b4b701e7845267b4c74005c1d4b8e4a52160ab
Matheus Batista <mbatista.sarti@gmail.com>
2024-11-02 16:25:14 -0300

    feat(basket): add feature StoreBasket

---
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,24 @@
+namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketCommandValidator()
+    {
+        RuleFor(command => command.Cart).NotNull().WithMessage("Cart can not be null");
+        RuleFor(command => command.Cart.Username).NotEmpty().WithMessage("Username is required");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+        //TODO: store basket in database
+        //TODO: update cache
+        return new StoreBasketResult(command.Cart.Username);
+    }
+}
