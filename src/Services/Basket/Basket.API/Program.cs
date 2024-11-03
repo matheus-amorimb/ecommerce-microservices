aa39d5422581a7fdbc4e8f1882880987b1e24b04
Matheus Batista <mbatista.sarti@gmail.com>
2024-11-03 16:30:35 -0300

    feat(basket): add health check for postgres and redis

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,6 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 
@@ -14,10 +17,22 @@ builder.Services.AddMediatR(configuration =>
 builder.Services
     .AddMarten(options =>
         {
-            options.Connection(builder.Configuration.GetConnectionString("Default") ?? string.Empty);
+            options.Connection(builder.Configuration.GetConnectionString("Database") ?? string.Empty);
             options.Schema.For<ShoppingCart>().Identity(cart => cart.Username);
         }
     ).UseLightweightSessions();
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+builder.Services
+    .AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database") ?? string.Empty)
+    .AddRedis(builder.Configuration.GetConnectionString("Redis") ?? string.Empty);
+
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
@@ -26,6 +41,12 @@ app.UseSwagger();
 app.UseSwaggerUI();
 app.MapCarter();
 app.UseHttpsRedirection();
+app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health", 
+    new HealthCheckOptions
+    {
+        ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    });
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
