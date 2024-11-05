ca323816f04b02990632e6c60868a38b6d5b7490
Matheus Batista <mbatista.sarti@gmail.com>
2024-11-04 22:32:54 -0300

    feat(basket): add method to communicate with grpc server

--- src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ src/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,3 +1,5 @@
+using Discount.Grpc;
+
 namespace Basket.API.Basket.StoreBasket;
 
 public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
@@ -12,12 +14,24 @@ public class StoreBasketCommandValidator : AbstractValidator<StoreBasketCommand>
     }
 }
 
-public class StoreBasketCommandHandler (IBasketRepository basketRepository) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+public class StoreBasketCommandHandler (IBasketRepository basketRepository, DiscountProtoService.DiscountProtoServiceClient discountProtoServiceClient) : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
     {
+        await ApplyDiscount(command.Cart, cancellationToken);
         var basket = await basketRepository.StoreBasket(command.Cart, cancellationToken);
         return new StoreBasketResult(basket.Username);
-        //TODO: update cache
     }
+    
+    private async Task ApplyDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = item.ProductName };
+            var coupon = await discountProtoServiceClient.GetDiscountAsync(discountRequest, cancellationToken: cancellationToken);
+            Console.WriteLine($"coupon.Amount {coupon.Amount}");
+            item.Price -= coupon.Amount;
+        }
+    }
+    
 }
