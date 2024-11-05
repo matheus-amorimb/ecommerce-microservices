5ae7f3dd7f97ef2165b80c7387d8a9c7a1b0ce63
Matheus Batista <mbatista.sarti@gmail.com>
2024-11-04 22:34:43 -0300

    chore(*): overall adjustments

--- src/Services/Discount/Discount.Grpc/GlobalUsing.cs
+++ src/Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -1,2 +1,6 @@
 global using Microsoft.EntityFrameworkCore;
 global using Discount.Grpc.Models;
+global using Discount.Grpc.Data;
+global using Discount.Grpc.Shared;
+global using Grpc.Core;
+global using Mapster;
