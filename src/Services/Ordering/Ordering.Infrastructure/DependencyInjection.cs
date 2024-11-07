28ae2ad09e47eb3ba8d92f40e2f48798e24ba67f
Matheus Batista <mbatista.sarti@gmail.com>
2024-11-07 19:52:56 -0300

    feat(db): inject sql server db service

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,18 @@
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ordering.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
+        var connectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString)
+                .UseSnakeCaseNamingConvention();
+        });
         return services;
     }
 }
