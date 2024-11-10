fb69402a20c5567208bbf3e884a36eb43bbf9ada
Matheus Batista <mbatista.sarti@gmail.com>
2024-11-10 20:53:47 -0300

    feat(*): add service for AppDbContext

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure;
@@ -20,6 +21,7 @@ public static class DependencyInjection
             options.UseSqlServer(connectionString)
                 .UseSnakeCaseNamingConvention();
         });
+        services.AddScoped<IAppDbContext, AppDbContext>();
         return services;
     }
 }
